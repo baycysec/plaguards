9a2caa2c4cf8dad069059b284ca80535044be5b4
LS <74954683+LawsonSchwantz@users.noreply.github.com>
2024-09-15 21:31:39 +0700

    Add files via upload

--- WEB-SERVER/web_project/plaguards/views.py
+++ WEB-SERVER/web_project/plaguards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from GuardModules.PlagFilter import *
 
 # Create your views here.
 def index(request): 
@@ -24,3 +25,21 @@ def tutorial(request):
         'title': 'Tutorial',
     }
     return render(request, 'tutorial.html', context)
+
+def file_upload(request):
+    context = {}
+    if request.method == 'POST':
+        file = request.FILES['file']
+        if not file:
+            context['message'] = "No file uploaded. Please choose a file to upload."
+            return render(request, 'tools.html', context)
+        if validate_file_extension(file) == False:
+            context['message'] = "Invalid file extension, please upload .ps1 or .txt files extension."
+            return render(request, 'tools.html', context)
+
+def search(request):
+    if request.method == 'POST':
+        searches = request.POST.get('search-bar', '')
+        result = search_sanitize(searches)
+        #ini nanti send ke fungsi pencarian ke MalwareBazaarya selanjutnya
+
