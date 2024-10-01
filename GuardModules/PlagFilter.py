cb5195a7432d2d0e1b7c686476193abc808edede
LS <74954683+LawsonSchwantz@users.noreply.github.com>
2024-10-01 07:46:18 -0400

    Update PlagFilter.py

--- GuardModules/PlagFilter.py
+++ GuardModules/PlagFilter.py
@@ -1,10 +1,10 @@
 import html
-import os
 
-def validate_file_extension(file):
-    valid_extensions = ['.ps1', '.txt']
-    ext = os.path.splitext(file.name)[1]
-    return ext.lower() in valid_extensions
+
+def validate_file_extension(filename):
+    if not filename.endswith(('.ps1', '.txt')):
+        return False
+    return True
 
 def search_sanitize(searches):
     safesearch = html.escape(searches)
