f295b1ec64d17261b3a6e0576e7c56c8b2d8f021
LS <74954683+LawsonSchwantz@users.noreply.github.com>
2024-10-27 00:11:31 +0700

    Update urls.py

--- web_project/urls.py
+++ web_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from plaguards.views import index, tools, about, tutorial, search, file_upload
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("index/", index, name="index"),
