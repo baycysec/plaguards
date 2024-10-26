93a7e7f57824abaa214a1cefad3cc3a2db8ecb99
LS <74954683+LawsonSchwantz@users.noreply.github.com>
2024-10-27 01:05:54 +0700

    Update urls.py

--- web_project/urls.py
+++ web_project/urls.py
@@ -27,8 +27,6 @@ urlpatterns = [
     path("tutorial/", tutorial, name="tutorial"),
     path("search/", search, name="search"),
     path("file_upload/", file_upload, name="file_upload"),
-    path("redirect_result/", search, name="ioc"),
-    path("redirect_result/", file_upload, name="deobfus"),
     path("", index, name="index"),
     # path('results/checker_result.pdf', serve, {'document_root': os.path.join(settings.BASE_DIR, 'results'), 'path': 'checker_result.pdf'}),
     path('redirect_result/', redirect_result, name="redirect_result"),
