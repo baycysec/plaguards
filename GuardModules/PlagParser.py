6f037dd8d9f1a85db8401f532818e854c7134203
Nicolas Saputra Gunawan <saputranicolas96@gmail.com>
2024-10-31 13:02:29 -0700

    Adding new fields for hash & signature query

--- GuardModules/PlagParser.py
+++ GuardModules/PlagParser.py
@@ -5,7 +5,6 @@ import base64
 import string
 import random
 
-
 def generate_random_val(length):
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(length))
