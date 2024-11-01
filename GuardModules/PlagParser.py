b36bb6aef628135f2065012ba34524e26c3a82cd
LS <74954683+LawsonSchwantz@users.noreply.github.com>
2024-11-01 13:59:32 +0700

    Update PlagParser.py

--- GuardModules/PlagParser.py
+++ GuardModules/PlagParser.py
@@ -30,14 +30,14 @@ def FindQuery(query_type, query_value):
     elif query_type == 'domain':
         url = f'https://www.virustotal.com/api/v3/domains/{query_value}'
         headers = {
-            'x-apikey': '685ca79fa45028696c796f773802c5cef7f495b9e63d74e817db0545701c029f'
+            'x-apikey': '<INSERT HERE>'
         }
         response = requests.get(url, headers=headers)
 
     elif query_type == 'ip':
         url = f'https://www.virustotal.com/api/v3/ip_addresses/{query_value}'
         headers = {
-            'x-apikey': '685ca79fa45028696c796f773802c5cef7f495b9e63d74e817db0545701c029f'
+            'x-apikey': '<INSERT HERE>'
         }
         response = requests.get(url, headers=headers)
 
@@ -45,7 +45,7 @@ def FindQuery(query_type, query_value):
         url_id = base64.urlsafe_b64encode(query_value.encode()).decode().strip("=")
         url = f'https://www.virustotal.com/api/v3/urls/{url_id}'
         headers = {
-            'x-apikey': '685ca79fa45028696c796f773802c5cef7f495b9e63d74e817db0545701c029f'
+            'x-apikey': '<INSERT HERE>'
         }
         response = requests.get(url, headers=headers)
 
